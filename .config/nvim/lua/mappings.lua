a575c802b3a70144d40d2cfdd892027aaa7c8e57
TecTrixer <tonihoevedes@gmail.com>
2022-11-21 22:53:40 +0100

    correct window after closing term

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,11 +13,11 @@ vim.api.nvim_set_keymap('n', '<Leader>gc', ':G commit -m "', {noremap = true})
 
 -- Running programs (Makefiles, Rust programs, ...)
 vim.api.nvim_create_autocmd('TermOpen', { pattern = '*', command = 'startinsert'})
-vim.api.nvim_set_keymap('n', '<Leader>cc', ':w<CR>:bo 20sp | term make<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>ct', ':w<CR>:bo 20sp | term cargo test<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>cd', ':w<CR>:bo 20sp | term cargo run<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>cr', ':w<CR>:bo 20sp | term cargo run --release<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>cb', ':w<CR>:bo 20sp | term cargo build --release<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>cc', ':w<CR>:bel 20sp | term make<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>ct', ':w<CR>:bel 20sp | term cargo test<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>cd', ':w<CR>:bel 20sp | term cargo run<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>cr', ':w<CR>:bel 20sp | term cargo run --release<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>cb', ':w<CR>:bel 20sp | term cargo build --release<CR>', {noremap = true})
 
 -- Automatically close references window
 vim.api.nvim_create_autocmd("FileType", {
